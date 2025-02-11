4c6537a9cf6f64d0e97000006e2ab590805c5915
Michael Peng <michael@mcmoo.org>
2025-02-11 12:05:40 -0800

    Add header guard exercise

---
+++ headers/queries.h
@@ -0,0 +1,6 @@
+// This module re-implements getQueries from lab 4 using the string_span struct from span.h.
+
+// Necessary to refer to "struct string_span"
+#include "span.h"
+
+struct string_span getQueries(char *url);
