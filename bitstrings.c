f0d7d023dcae35255b907110161200a68cf8fd6d
Peng <h4peng@ieng6-201.ucsd.edu>
2025-02-11 16:34:48 -0800

    Reduce scope of bitstrings exercise

--- bitstrings.c
+++ bitstrings.c
@@ -2,29 +2,32 @@
 
 // set the nth bit of b to bit
 // example: set_nth_bit(0b101, 1, 1) => 0b111
-size_t set_nth_bit(size_t b, int n, int bit) { return 0; }
+size_t set_nth_bit(size_t b, int n, int bit) {
+    // TODO
+}
 
 // return the subbitstring, where start is the least significant bit
 // (inclusive), and end is the most significant bit (inclusive)
 // example: sub_bitstring(0b101101, 2, 6) => 0b1011
-size_t sub_bitstring(size_t b, size_t start, size_t end) { return 0; }
-
-// given a bitmask of size mask_size, apply the mask to b repeatedly, moving up
-// the position of the mask until the mask has been applied to the whole size_t.
-// Assume that the size of size_t is 32 for this exercise example:
-// loop_mask(0b0, 0b101, 3) => 0b01101...101101
-size_t loop_mask(size_t b, size_t mask, size_t mask_size) { return 0; }
+size_t sub_bitstring(size_t b, size_t start, size_t end) {
+    // TODO
+}
 
 void print_bin(size_t n) {
-  while (n) {
-    if (n & 1)
-      printf("1");
-    else
-      printf("0");
+  int i;
+  unsigned int mask = 1 << (sizeof(int) * 8 - 1);
 
-    n >>= 1;
+  for (i = 0; i < sizeof(int) * 8; i++) {
+    if (n & mask) { 
+      printf("1");
+    } else {
+      printf("0");
+    }
+    mask >>= 1;
   }
   printf("\n");
 }
 
-int main(void) { return 0; }
+int main(void) {
+    // Test things out here
+}
